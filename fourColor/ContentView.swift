5c326e7e93c38e49a24ed9c63bfe0670c296d708
nataliawcislo <wcislonatalia1998@gmail.com>
2020-04-08 11:50:07 +0200

    Initial Commit

---
+++ fourColor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fourColor
+//
+//  Created by Natalia Wcisło on 08/04/2020.
+//  Copyright © 2020 Natalia Wcisło. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
