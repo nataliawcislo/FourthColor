d54d166143a7275b088eac0f6260c319e8f9def6
nataliawcislo <wcislonatalia1998@gmail.com>
2020-04-12 23:25:00 +0200

    Added NavBar

---
+++ fourColor/MenuCard.swift
@@ -0,0 +1,21 @@
+//
+//  MenuCard.swift
+//  fourColor
+//
+//  Created by Natalia Wcisło on 12/04/2020.
+//  Copyright © 2020 Natalia Wcisło. All rights reserved.
+//
+
+import SwiftUI
+
+struct MenuCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuCard_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuCard()
+    }
+}
